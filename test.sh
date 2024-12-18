17addd545629246539375a4d18516b3f785cd627
Isaac Lins <contact@isaaclins.com>
2024-12-18 16:46:37 +0100

    Aktualisiere das Skript zur Generierung der Commit-Historie mit neuem Datumsformat

--- test.sh
+++ test.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 # Get the commits by date and format them
-git log --pretty=format:'## %ad%n%h %s' --date=iso > commits.txt
-
-# Check if README.md is empty
-if [ ! -s README.md ]; then
-  cat commits.txt > README.md
-else
-  # Append the formatted commits to the 11th line of README.md
-  awk 'NR==11{while((getline line < "commits.txt") > 0) print line}1' README.md > temp && mv temp README.md
-fi
+git log --pretty=format:'## %ad%n%h %s' --date=format:'%Y-%m-%d' > commits.txt
